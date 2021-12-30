9cc0531f635a0dccb6eabcbdd35d34267cf9688e
Ines Gaspar <insagaspar08@gmail.com>
2021-12-30 15:48:02 +0000

    Change constructor menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -35,7 +35,7 @@ public class Menu {
         terminalSize = new TerminalSize(80, 60);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
         terminal = terminalFactory.createTerminal();
-        Screen screen = new TerminalScreen(terminal);
+        screen = new TerminalScreen(terminal);
 
         screen.setCursorPosition(null); // we don't need a cursor
         screen.startScreen(); // screens must be started
