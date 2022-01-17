3b7d75a6e4abb34de8b46be8bb6dc1cade44dc49
Ines Gaspar <insagaspar08@gmail.com>
2022-01-17 19:16:55 +0000

    Add ScatterBottomLeft and ScatterBottomRight

--- src/main/java/ScatterBottomLeft.java
+++ src/main/java/ScatterBottomLeft.java
@@ -1,6 +1,56 @@
 public class ScatterBottomLeft implements Scatter{
     @Override
     public Position scatter(Position ghostP) {
-        return null;
+        Maze maze = new Maze();
+        int x = ghostP.getX(), y = ghostP.getY();
+        Position newP = ghostP;
+        //Position c1 = new Position(1, 27), c2 = new Position(16, 30);
+
+        if ((x >= 1 && x <= 6) || (x >= 9 && x <= 13) || (y >= 27 && y <= 30)){     //verifica se está no quadrante
+            if (!maze.isWall(new Position(x, y+1)) && ((y + 1 >= 27))){     //verifica se pode mover-se para cima
+                newP = new Position(x, y+1);
+            }
+
+            else if (!maze.isWall(new Position(x-1, y) )&& ((x-1 >= 1) || (x-1 >= 9))){     //verifica se pode mover-se para a esquerda
+                newP = new Position(x-1, y);
+            }
+        }
+
+        else {
+            if (!maze.isWall(new Position(x, y+1))){
+                newP = new Position(x, y+1);
+            }
+
+            else if (!maze.isWall(new Position(x-1, y))){
+                newP = new Position(x-1, y);
+            }
+
+            else {
+                int outcome = (int)Math.random()*2 + 1;
+
+                switch(outcome){
+                    case 1:
+                        if (!maze.isWall(new Position(x, y-1))){
+                            newP = new Position(x, y-1);
+                        }
+
+                        else if (!maze.isWall(new Position(x+1, y))){
+                            newP = new Position(x+1, y);
+                        }
+
+                    case 2:
+                        if (!maze.isWall(new Position(x+1, y))){
+                            newP = new Position(x+1, y);
+                        }
+
+                        else if (!maze.isWall(new Position(x, y-1))){
+                            newP = new Position(x, y-1);
+                        }
+                }
+            }
+
+        }
+
+        return newP;
     }
 }
--- src/main/java/ScatterBottomRight.java
+++ src/main/java/ScatterBottomRight.java
@@ -1,6 +1,55 @@
 public class ScatterBottomRight implements Scatter {
     @Override
     public Position scatter(Position ghostP) {
-        return null;
+        Maze maze = new Maze();
+        int x = ghostP.getX(), y = ghostP.getY();
+        Position newP = ghostP;
+        Position c1 = new Position(16, 27), c2 = new Position(23, 30);
+                //20                                            //29
+
+        if ((x >= 16 && x <= 20) || (x >= 23 && x <=29) || (y >= 27 && x <= 30)){
+            if (!maze.isWall(new Position(x, y+1)) && (y + 1 >= 27)){
+                newP = new Position(x, y+1);
+            }
+
+            else if (!maze.isWall(new Position(x+1, y)) && ((x+1 >= 16) || (x+1 >= 23))){
+                newP = new Position(x+1, y);
+            }
+        }
+
+        else {
+            if (!maze.isWall(new Position(x, y+1))){
+                newP = new Position(x, y+1);
+            }
+
+            else if (!maze.isWall(new Position(x+1, y))){
+                newP = new Position(x+1, y);
+            }
+
+            else {
+                int outcome = (int)Math.random()*2 + 1;
+
+                switch(outcome){
+                    case 1:
+                        if (!maze.isWall(new Position(x, y-1))){
+                            newP = new Position(x, y-1);
+                        }
+
+                        else if (!maze.isWall(new Position(x+1, y))){
+                            newP = new Position(x+1, y);
+                        }
+
+                    case 2:
+                        if (!maze.isWall(new Position(x+1, y))){
+                            newP = new Position(x+1, y);
+                        }
+
+                        else if (!maze.isWall(new Position(x, y-1))){
+                            newP = new Position(x, y-1);
+                        }
+                }
+            }
+        }
+        return newP;
     }
 }
